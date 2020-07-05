6e0a02961883727e9dfce3081484ac4eaa8671d2
Huy Pham <p.q.huy97@gmail.com>
2020-07-06 00:09:18 +0700

    Remove footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,15 +26,6 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className={cx(s.root, this.props.className)}>
-        <div className={s.container}>
-          <span>© 2019 &nbsp;Flatlogic LLC </span>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/tos">Terms of Service</Link>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/privacy">Privacy Policy</Link>
-          <span className={s.spacer}>·</span>
-          <Link to="/app/not-found">Support</Link>
-        </div>
       </footer>
     );
   }
